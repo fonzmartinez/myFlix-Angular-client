90269d693e181855491ea402097915157a7d2cd9
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-05-30 19:49:05 -0500

    Implemented user registration

--- myFlix-Angular-client/src/app/fetch-api-data.service.ts
+++ myFlix-Angular-client/src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 
-const apiUrl = 'https://enigmatic-eyrie-99477.herokuapp.com';
+const apiUrl = 'https://enigmatic-eyrie-99477.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
