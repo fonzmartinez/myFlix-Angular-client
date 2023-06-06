ceb1a4033cbe99032b604d188cd7baf6416f5dd0
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-06-05 23:46:17 -0500

    Added welcome page and movie card components

---
+++ myFlix-Angular-client/src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss']
+})
+export class MovieCardComponent {
+  movies: any[] = [];
+  constructor(public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+
+}
